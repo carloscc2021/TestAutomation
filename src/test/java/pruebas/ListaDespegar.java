dbff8ca889cad9b1eacd852ee7f37cf567360288
Carlos <carloscctorres17@gmail.com>
2022-02-28 22:59:39 -0300

    refactor and add selenium driver

--- src/test/java/pruebas/ListaDespegar.java
+++ src/test/java/pruebas/ListaDespegar.java
@@ -12,16 +12,16 @@ import java.util.List;
 public class ListaDespegar {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","/home/carlos/Documents/drivers/chrome/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./drivers/chrome/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.despegar.com.ar/");
-        Thread.sleep(500);
+
         List<WebElement> lista = driver.findElements(By.xpath("//label[@class='button-circle-label']"));
-        for (int i = 0; i < lista.size(); i++){
-        System.out.println(lista.get(i).getText());
-        Assert.assertTrue(lista.get(i).isDisplayed());
-        Thread.sleep(500);
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i).getText());
+            Assert.assertTrue(lista.get(i).isDisplayed());
         }
+
         driver.quit();
     }
 }
